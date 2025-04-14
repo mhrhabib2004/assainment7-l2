edf8989c20df34879eaa5428facf115b94b804dd
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-04-14 11:57:04 +0600

    insert customers data

--- tableCreation/customers.sql
+++ tableCreation/customers.sql
@@ -4,4 +4,13 @@ CREATE TABLE customers (
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
     joined_date DATE DEFAULT CURRENT_DATE
-)
+);
+
+
+-- Insert data into customers table
+INSERT INTO customers (name, email, joined_date) VALUES
+('Alice', 'alice@email.com', '2023-01-10'),
+('Bob', 'bob@email.com', '2022-05-15'),
+('Charlie', 'charlie@email.com', '2023-06-20');
+
+
